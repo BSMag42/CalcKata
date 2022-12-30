d9952d29c9ca404f6c876fd01989520b4834d004
SMagennis <sinead.magennis@ao.com>
2022-12-30 11:45:16 +0000

    Added second test for 2 numbers

--- CalcKata/StringCalculator.cs
+++ CalcKata/StringCalculator.cs
@@ -1,9 +1,27 @@
-﻿namespace CalcKata;
+﻿using System;
+
+namespace CalcKata;
 public class StringCalculator
 {
     public StringCalculator()
     {}
-    public int Add(string param1)
-    { return 0; }
+
+    public int Add(string param1, string param2)
+    {
+        var result = 0;
+        if (param1 != "")
+        {
+            var int1 = int.Parse(param1);
+            result += int1;
+        }
+
+        if (param2 != "")
+        {
+            var int2 = int.Parse(param2);
+            result += int2;
+        }
+            
+        return result;
+    }
 
 }
--- TestCalcKata/StringCalculatorTests.cs
+++ TestCalcKata/StringCalculatorTests.cs
@@ -9,14 +9,19 @@ namespace TestCalcKata
         {
             //arrange
             var sut = new StringCalculator();
-
             //act
-            var result = sut.Add("");
-
+            var result = sut.Add("","");
             //result
             Assert.Equal(0,result);
         }
 
+        [Fact]
+        public void Returns_Sum_of_Two_Numbers()
+        {
+            var sut = new StringCalculator();
+            var result = sut.Add("3", "2");
+            Assert.Equal(5,result);
+        }
 
     }
 }
